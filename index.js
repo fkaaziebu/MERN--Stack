92f9f7a6b472e41cd3c8d76d83fe98806c6edcda
fkaaziebu <frederickaziebu1998@gmail.com>
2023-05-17 11:43:09 +0000

    Session 1
    Intro to Javascript
    
    Setting up react

---
+++ index.js
@@ -0,0 +1,43 @@
+// Statements
+
+// type name = value
+const username = "Frederick Aziebu"
+console.log("My name is: " + username);
+
+// Variables
+// type name = value
+const age = 10;
+
+let ageofuser = 10;
+ageofuser = 9
+
+// Loops
+for (let i = 0; i < 4; i++) {
+    // code to run
+} 
+// 0 -> 1 -> 2 -> 3 -> 4
+let i = 0
+while (i < 4) {
+    // code to run
+    i++
+}
+
+arr = [1, 2, 3, 4]
+
+arr.map((val) => {
+    return val % 2 == 0;
+});
+
+[2, 4]
+
+// Functions
+function Sum (a, b) {
+    return a + b;
+}
+Sum(3, 4) // 7
+
+// Arrow Functions
+const Add = (a, b) => {
+    return a + b;
+}
+Add(3, 4) // 7
